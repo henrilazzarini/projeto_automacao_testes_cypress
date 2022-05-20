5cc9e27c75ce20697efdf47fc44b744528f4b8da
Henrique Viciedo <henrique.viciedo@gmail.com>
2022-05-20 10:37:56 -0300

    Criação do projeto e automações de teste na página de registro de usuário

---
+++ cypress/integration/3-alurapic/alurapic.spec.js
@@ -0,0 +1,49 @@
+describe('Login e registro de usuários alura pic', () => {
+
+    beforeEach(() => {
+        cy.visit('https://alura-fotos.herokuapp.com');
+    })
+
+    it('verifica mensagens de campos obrigatórios no registro', () => {
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Email is required!').should('be.visible');
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Full name is required!').should('be.visible');
+        cy.contains('ap-vmessage', 'User name is required!').should('be.visible');
+        cy.contains('ap-vmessage', 'Password is required!').should('be.visible');
+    })
+
+    it('verifica mensagem de email invalido', () => {
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.get('input[formcontrolname="email"]').type('Henrique');
+        cy.contains('ap-vmessage', 'Invalid e-mail').should('be.visible');
+    })
+
+    it ('verifica mensagem de nome completo com mais de 60 caracteres', () => {
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.get('input[formcontrolname="fullName"]').type('Pedro de Alcântara Francisco Antônio João Carlos Xavier de Paula Miguel Rafael Joaquim José Gonzaga Pascoal Cipriano Serafim de Bragança e Bourbon');
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Maximun length is 40').should('be.visible');     
+    })
+
+    it('verifica mensagem de user name com caracter maiúsculo', () => {
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.get('input[formcontrolname="userName"]').type('HenriqueLazzarini');
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Must be lower case').should('be.visible');        
+    })
+
+    it('verifica mensagem de senha com menos de 8 caracteres', () => {
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.get('input[formcontrolname="password"]').type('123');
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Mininum length is 8').should('be.visible');        
+    })
+    
+
+} )
